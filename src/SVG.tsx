6fc220193a38fc6720f6edfdb80cbc0270bcf415
bucketmedia <61097620+bucketmedia@users.noreply.github.com>
2024-02-20 18:06:12 +0000

    fix: remove useMemo

--- src/SVG.tsx
+++ src/SVG.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import type { SVGData } from './types'
 import { v4 as uuid } from 'uuid'
 
@@ -9,13 +9,10 @@ type Props = {
 const SVG = ({ icon }: Props) => {
   const { iconName, paths, ...svgProps } = icon
 
-  const keys = useMemo(() => {
-    return paths.map(() => uuid())
-  }, [paths])
   return (
     <svg {...svgProps}>
       {paths.map((path, i) => (
-        <path {...path} key={keys[i]} />
+        <path {...path} key={uuid()} />
       ))}
     </svg>
   )

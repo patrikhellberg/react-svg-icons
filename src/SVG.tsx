72f28390b9ae02d2978e5073df37f91483a9ccf5
bucketmedia <61097620+bucketmedia@users.noreply.github.com>
2024-02-20 18:08:52 +0000

    fix: add default path stroke

--- src/SVG.tsx
+++ src/SVG.tsx
@@ -4,15 +4,16 @@ import { v4 as uuid } from 'uuid'
 
 type Props = {
   icon: SVGData
+  stroke?: string
 }
 
-const SVG = ({ icon }: Props) => {
+const SVG = ({ icon, stroke = 'black' }: Props) => {
   const { iconName, paths, ...svgProps } = icon
 
   return (
     <svg {...svgProps}>
       {paths.map((path, i) => (
-        <path {...path} key={uuid()} />
+        <path {...path} key={uuid()} stroke={stroke} />
       ))}
     </svg>
   )

d87d0bd10a3268f7a5225d095a55b1eaf197c2af
bucketmedia <61097620+bucketmedia@users.noreply.github.com>
2024-02-21 12:26:32 +0000

    fix: wierd dependency thing

---
+++ preview/app/components/IconCard.tsx
@@ -0,0 +1,7 @@
+// import {} from "@bm-js/icons"
+
+const IconCard = () => {
+  return <div>IconCard</div>
+}
+
+export default IconCard
--- preview/app/components/IconResults.tsx
+++ preview/app/components/IconResults.tsx
@@ -1,18 +1,21 @@
-import * as _icons from '@bm-js/icons'
+// import * as _icons from '@bm-js/icons'
+// import {} from "@bm-js/icons"
 
 const IconResults = () => {
-  const { default: SVG, ...icons } = _icons
-  const iconKeys = Object.keys(icons).filter(
-    (k) => k !== 'default'
-  ) as unknown as (keyof typeof icons)[]
+  // console.log(_icons)
+  return <p>test</p>
+  //   const { default: SVG, ...icons } = _icons
+  //   const iconKeys = Object.keys(icons).filter(
+  //     (k) => k !== 'default'
+  //   ) as unknown as (keyof typeof icons)[]
 
-  return (
-    <div>
-      {iconKeys.map((key) => (
-        <SVG key={`icon_${key}`} icon={icons[key] as any} />
-      ))}
-    </div>
-  )
+  //   return (
+  //     <div>
+  //       {iconKeys.map((key) => (
+  //         <SVG key={`icon_${key}`} icon={icons[key] as any} />
+  //       ))}
+  //     </div>
+  //   )
 }
 
 export default IconResults
